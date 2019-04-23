924688e31a4c5e6eae8b8eabd8a6a2f950207641
Skylar <skylarhimeno@gmail.com>
2019-04-23 09:53:09 -0600

    updated chattwitter

--- src/chat/model/ChatTwitter.java
+++ src/chat/model/ChatTwitter.java
@@ -1,9 +1,16 @@
 package chat.model;
 
 import chat.controller.ChatController;
+import twitter4j.Twitter;
 
 public class ChatTwitter
 {
-	private 
+	private ChatController app;
+	private Twitter ChatTwitter;
+	
+	public ChatTwitter(ChatController app)
+	{
+		this.app = app;
+	}
 
 }
